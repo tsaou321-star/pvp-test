362764f0084d2821b6b75eed1e28b81e8da2539c
tsaou321 <tsaou321@gmail.com>
2026-06-30 18:52:43 +0800

    fix server

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
